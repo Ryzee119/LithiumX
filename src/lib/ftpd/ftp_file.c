Ryzee119/LithiumX

commit 386ca39d2b0270e4eb6b19b6e34ba9621d27306f

ftp: Extend and index root drives with nxIsDriveMounted

src/lib/ftpd/ftp_file.c
@@ -15,13 +15,15 @@
 #include <stdlib.h>
 #include <assert.h>
 
+#include <nxdk/mount.h>
+
 #ifndef INVALID_HANDLE_VALUE
 #define INVALID_HANDLE_VALUE ((HANDLE) (LONG_PTR)-1)
 #endif
 
 #ifdef NXDK
 #include <xboxkrnl/xboxkrnl.h>
-static const char root_drives[][3] = {"/C", "/D", "/E", "/F", "/G", "/X", "/Y", "/Z"};
+static const char root_drives[][3] = {"/C", "/D", "/E", "/F", "/G", "/R", "/S", "/V", "/W", "/A", "/B", "/P", "/X", "/Y", "/Z"};
 static int root_index;
 #define FILE_DBG DbgPrint
 #else
@@ -211,9 +213,15 @@ FRESULT ftps_f_readdir(DIR *dp, FILINFO *nfo)
 #ifdef FTP_CUSTOM_ROOT_PATH
 	if (strcmp(dp->path, "root") == 0)
 	{
-		if (root_index < (sizeof(root_drives) / sizeof(root_drives[0])))
+		// null terminate the filename in case we don't find anything
+		nfo->fname[0] = '\0';
+
+		for(root_index; root_index < (sizeof(root_drives) / sizeof(root_drives[0])); root_index++)
 		{
-			nfo->fname[0] = root_drives[root_index++][1];
+			if(!nxIsDriveMounted(root_drives[root_index][1]))
+				continue;
+
+			nfo->fname[0] = root_drives[root_index][1];
 			nfo->fname[1] = '\0';
 			nfo->fattrib = AM_DIR;
 
@@ -222,11 +230,11 @@ FRESULT ftps_f_readdir(DIR *dp, FILINFO *nfo)
 			nfo->fdate |= 1 << 5; //Jan
 			nfo->fdate |= 1 << 0; //1st
 			nfo->ftime = 0;
+
+			// Break out of the loop once we find a valid drive
+			break;
 		}
-		else
-		{
-			nfo->fname[0] = '\0';
-		}
+
 		return FR_OK;
 	}
 #endif

commit cec72abc1b682cfbba7f1b987383e1f44e93f5ee

ftp: Fix root dir bug introduced in 386ca39

src/lib/ftpd/ftp_file.c
@@ -216,7 +216,7 @@ FRESULT ftps_f_readdir(DIR *dp, FILINFO *nfo)
 		// null terminate the filename in case we don't find anything
 		nfo->fname[0] = '\0';
 
-		for(root_index; root_index < (sizeof(root_drives) / sizeof(root_drives[0])); root_index++)
+		for(; root_index < (sizeof(root_drives) / sizeof(root_drives[0])); root_index++)
 		{
 			if(!nxIsDriveMounted(root_drives[root_index][1]))
 				continue;
@@ -234,6 +234,9 @@ FRESULT ftps_f_readdir(DIR *dp, FILINFO *nfo)
 			// Break out of the loop once we find a valid drive
 			break;
 		}
+
+		// Increment the root index for the next call
+		root_index++;
 
 		return FR_OK;
 	}