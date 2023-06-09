6d13b59ba24e11ae441188c7bf18837980e1d2df
Ryan Wendland <wendland@live.com.au>
2023-06-09 12:42:33 +0930

    Styles: Adjust styles

--- src/dash_styles.c
+++ src/dash_styles.c
@@ -1,30 +1,30 @@
 // SPDX-License-Identifier: MIT
 // SPDX-FileCopyrightText: 2022 Ryzee119
 
-#include "lvgl.h"
-#include "dash.h"
-#include "dash_styles.h"
+#include "lithiumx.h"
 
 lv_style_t dash_background_style;
 lv_style_t menu_table_style;
 lv_style_t menu_table_highlight_style;
 lv_style_t menu_table_cell_style;
-lv_style_t synop_container_style;
-lv_style_t synop_text_style;
+lv_style_t object_style;
 lv_style_t titleview_style;
 lv_style_t titleview_image_container_style;
 lv_style_t titleview_image_text_style;
 lv_style_t titleview_header_footer_style;
-lv_style_t eeprom_items_style;
+lv_color_t dash_base_theme_color;
 
-void dash_styles_init(void)
+void dash_styles_init(lv_color_t theme_colour)
 {
+    dash_base_theme_color = theme_colour;
+
+    //lv_color_make(22, 111, 15)
     // Set the style for the background
     lv_style_init(&dash_background_style);
     lv_style_set_border_width(&dash_background_style, 0);
     lv_style_set_radius(&dash_background_style, 0);
     lv_style_set_bg_color(&dash_background_style, lv_color_make(15, 15, 15));
-    lv_style_set_bg_grad_color(&dash_background_style, lv_color_make(22, 111, 15));
+    lv_style_set_bg_grad_color(&dash_background_style, theme_colour);
     lv_style_set_bg_grad_dir(&dash_background_style, LV_GRAD_DIR_VER);
 
     // Set the style for the main menu container
@@ -52,23 +52,21 @@ void dash_styles_init(void)
     lv_style_set_radius(&menu_table_cell_style, 0);
     lv_style_set_outline_width(&menu_table_cell_style, 0);
 
+    // Set the style for the main menu container
+    lv_style_init(&object_style);
+    lv_style_set_bg_color(&object_style, lv_color_make(15, 15, 15));
+    lv_style_set_bg_opa(&object_style, 240);
+    lv_style_set_border_width(&object_style, 0);
+    lv_style_set_pad_all(&object_style, 0);
+    lv_style_set_radius(&object_style, 0);
+    lv_style_set_text_color(&object_style, lv_color_white());
+    lv_style_set_text_font(&object_style, &lv_font_montserrat_20);
+    lv_style_set_outline_width(&object_style, 0);
+    lv_style_set_text_line_space(&object_style, 10);
+
     // Set the style for the main menu item cells when they are selected
     lv_style_init(&menu_table_highlight_style);
-    lv_style_set_bg_color(&menu_table_highlight_style, lv_color_make(61, 153, 0));
-
-    // Set the style for the synopsis screen container
-    lv_style_init(&synop_container_style);
-    lv_style_set_bg_color(&synop_container_style, lv_color_make(15, 15, 15));
-    lv_style_set_bg_opa(&synop_container_style, 200);
-    lv_style_set_pad_left(&synop_container_style, 10);
-    lv_style_set_border_width(&synop_container_style, 1);
-    lv_style_set_radius(&synop_container_style, 0);
-
-    // Set the style for the synopsis screen text.
-    // To change the colour of the synop title text, edit DASH_MENU_COLOR in dash_styles.h
-    lv_style_init(&synop_text_style);
-    lv_style_set_text_color(&synop_text_style, lv_color_white());
-    lv_style_set_text_font(&synop_text_style, &lv_font_montserrat_20);
+    lv_style_set_bg_color(&menu_table_highlight_style, theme_colour);
 
     // Create a style for the container that holds all the images
     // Set padding between images, background colour behind the images etc
@@ -84,11 +82,10 @@ void dash_styles_init(void)
     // as we want this container to be invisible
     lv_style_init(&titleview_image_container_style);
     lv_style_set_radius(&titleview_image_container_style, 0);
-    lv_style_set_border_width(&titleview_image_container_style, 0);
     lv_style_set_bg_color(&titleview_image_container_style, lv_color_make(34, 34, 34));
-    lv_style_set_border_color(&titleview_image_container_style, lv_color_make(255, 255, 255));
+    lv_style_set_border_color(&titleview_image_container_style, lv_color_darken(theme_colour, 100));
     lv_style_set_pad_all(&titleview_image_container_style, 0);
-    lv_style_set_border_width(&titleview_image_container_style, 0);
+    lv_style_set_border_width(&titleview_image_container_style, 1);
 
     // Create a style for the text that appears on the thumbnail art when no artwork is found
     lv_style_init(&titleview_image_text_style);
@@ -99,21 +96,11 @@ void dash_styles_init(void)
 
     // Create a style for the page header and footer text
     lv_style_init(&titleview_header_footer_style);
-    lv_style_set_bg_color(&titleview_header_footer_style, lv_color_make(0, 72, 16));
+    lv_style_set_bg_color(&titleview_header_footer_style, theme_colour);
     lv_style_set_text_color(&titleview_header_footer_style, lv_color_white());
     lv_style_set_text_font(&titleview_header_footer_style, &lv_font_montserrat_26);
 
-    //Style for the eeprom setting screen
-    lv_style_init(&eeprom_items_style);
-    lv_style_set_border_width(&eeprom_items_style, 1);
-    lv_style_set_border_color(&eeprom_items_style, lv_color_white());
-    lv_style_set_bg_color(&eeprom_items_style, lv_color_make(15, 15, 15));
-    lv_style_set_shadow_opa(&eeprom_items_style, 0);
-    lv_style_set_text_color(&eeprom_items_style, lv_color_white());
-    lv_style_set_text_font(&eeprom_items_style, &lv_font_montserrat_20);
-    lv_style_set_color_filter_opa(&eeprom_items_style, 0);
-    lv_style_set_radius(&eeprom_items_style, 0);
-    lv_style_set_outline_width(&eeprom_items_style, 0);
+    lv_obj_mark_layout_as_dirty(lv_scr_act());
 }
 
 void dash_styles_deinit(void)
@@ -122,11 +109,9 @@ void dash_styles_deinit(void)
     lv_style_reset(&menu_table_style);
     lv_style_reset(&menu_table_highlight_style);
     lv_style_reset(&menu_table_cell_style);
-    lv_style_reset(&synop_container_style);
-    lv_style_reset(&synop_text_style);
+    lv_style_reset(&object_style);
     lv_style_reset(&titleview_style);
     lv_style_reset(&titleview_image_container_style);
     lv_style_reset(&titleview_image_text_style);
     lv_style_reset(&titleview_header_footer_style);
-    lv_style_reset(&eeprom_items_style);
 }
--- src/dash_styles.h
+++ src/dash_styles.h
@@ -8,21 +8,29 @@
 extern "C" {
 #endif
 
+#include "lithiumx.h"
+
 #define DASH_MENU_COLOR "#999999"
 
+#ifndef DASH_XMARGIN
+#define DASH_XMARGIN 20
+#endif
+#ifndef DASH_YMARGIN
+#define DASH_YMARGIN 20
+#endif
+
+extern lv_color_t dash_base_theme_color;
 extern lv_style_t dash_background_style;
 extern lv_style_t menu_table_style;
 extern lv_style_t menu_table_highlight_style;
 extern lv_style_t menu_table_cell_style;
-extern lv_style_t synop_container_style;
-extern lv_style_t synop_text_style;
+extern lv_style_t object_style;
 extern lv_style_t titleview_style;
 extern lv_style_t titleview_image_container_style;
 extern lv_style_t titleview_image_text_style;
 extern lv_style_t titleview_header_footer_style;
-extern lv_style_t eeprom_items_style;
 
-void dash_styles_init(void);
+void dash_styles_init(lv_color_t theme_colour);
 void dash_styles_deinit(void);
 
 #ifdef __cplusplus
