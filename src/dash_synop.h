a46a6c06055ea14f4adfe373f0bc33c590f3cfbc
Ryan Wendland <wendland@live.com.au>
2023-06-09 12:42:59 +0930

    Synop: Rework to use container helper

--- src/dash_synop.h
+++ src/dash_synop.h
@@ -1,18 +1,16 @@
 // SPDX-License-Identifier: MIT
 // SPDX-FileCopyrightText: 2022 Ryzee119
 
-#ifndef _SYNOP_H
-#define _SYNOP_H
+#ifndef _DASH_SYNOP_H
+#define _DASH_SYNOP_H
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#include "dash_titlelist.h"
+#include "lithiumx.h"
 
-void synop_menu_init(void);
-void synop_menu_deinit(void);
-void synop_menu_open(title_t *title);
+void dash_synop_open(int id);
 
 #ifdef __cplusplus
 }
