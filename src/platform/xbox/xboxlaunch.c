2fa4ffba900d22fd2c63d2effd165666ba2b59e6
Ryzee119 <wendland@live.com.au>
2023-06-20 20:25:15 +0930

    Xbox: Allow drive letters with multiple characters

--- src/platform/xbox/xboxlaunch.c
+++ src/platform/xbox/xboxlaunch.c
@@ -6,16 +6,21 @@
 #include <stdbool.h>
 #include <string.h>
 
-static bool symbolicLinkToXboxPath(const CHAR driveLetter, PCHAR fullPath, INT maximumLength)
+static bool symbolicLinkToXboxPath(const CHAR *driveLetter, INT driveLength, PCHAR fullPath, INT maximumLength)
 {
     NTSTATUS status;
     HANDLE symbolicLinkHandle;
     OBJECT_ATTRIBUTES symbolicLinkObject;
-    CHAR symbolicLinkBuffer[] = "\\??\\?:";
+    CHAR symbolicLinkBuffer[MAX_PATH];
+
+    // Path needs to be the the format "\??\X:" where X is the driveLetter
+    strcpy(&symbolicLinkBuffer[0], "\\??\\");
+    strncpy(&symbolicLinkBuffer[4], driveLetter, driveLength);
+    strcpy(&symbolicLinkBuffer[4 + driveLength], ":");
 
     OBJECT_STRING symbolicLinkString = {
-        .Length = sizeof(symbolicLinkBuffer) - 1,
-        .MaximumLength = sizeof(symbolicLinkBuffer),
+        .Length = strlen(symbolicLinkBuffer),
+        .MaximumLength = MAX_PATH,
         .Buffer = symbolicLinkBuffer
     };
 
@@ -24,8 +29,6 @@ static bool symbolicLinkToXboxPath(const CHAR driveLetter, PCHAR fullPath, INT m
         .MaximumLength = maximumLength - 1,
         .Buffer = fullPath
     };
-
-    symbolicLinkBuffer[4] = driveLetter;
 
     InitializeObjectAttributes(&symbolicLinkObject, &symbolicLinkString, OBJ_CASE_INSENSITIVE, NULL, NULL);
     status = NtOpenSymbolicLinkObject(&symbolicLinkHandle, &symbolicLinkObject);
@@ -46,18 +49,11 @@ static bool symbolicLinkToXboxPath(const CHAR driveLetter, PCHAR fullPath, INT m
     return true;
 }
 
-static bool isAlphaNumeric(const char character)
-{
-    return (character >= 'a' && character <= 'z') ||
-           (character >= 'A' && character <= 'Z') ||
-           (character >= '0' && character <= '9');
-}
-
 // Returns the first character that is A-Z or a-z from the start of the input string.
 static const char *firstAlphaNumericCharacter(const char *str)
 {
     while (*str != '\0') {
-        if (isAlphaNumeric(*str)) {
+        if (isalnum(*str)) {
             return str;
         }
         str++;
@@ -71,64 +67,65 @@ static const char *firstAlphaNumericCharacter(const char *str)
 **/
 int XboxGetFullLaunchPath(const char *input, char *output)
 {
-    // If the input path is already in 'Xbox format', copy it directly and leave
-    if (memcmp(input, "\\Device\\", 8) == 0) {
-        strcpy(output, input);
-        return STATUS_SUCCESS;
-    }
+    const char *driveLetter = NULL;
+    const char *pathStart = NULL;
 
-    const char *driveSeparator = strchr(input, ':');
-    const char *pathStart = input;
-    char driveLetter = '\0';
-
-    output[0] = '\0';
-
-    if (strlen(input) == 0)
+    int inputPathLen = strlen(input);
+    if (inputPathLen == 0)
     {
         return ERROR_BAD_PATHNAME;
     }
 
+    // If the input path is already in 'Xbox format', copy it directly and leave
+    if (memcmp(input, "\\Device\\", (inputPathLen < 8) ? inputPathLen : 8) == 0) {
+        strcpy(output, input);
+        return STATUS_SUCCESS;
+    }
+
+    // Get C:\foo\bar.txt from a path with prefixes like \\??\\C:\foo\bar.txt
+    const char *drivePathStart = firstAlphaNumericCharacter(input);
+    // Get :\foo\bar.text from C:\foo\bar.txt
+    const char *driveSeparator = strchr(drivePathStart, ':');
+
+    output[0] = '\0';
+
     // If the path has a ':', we assume "driveLetter:\pathStart"
     if (driveSeparator) {
-        // Paths like ":foo/path" will cause issues. Leave with error
-        if (driveSeparator == input) {
-            return ERROR_BAD_PATHNAME;
-        }
-
-        driveLetter = *(driveSeparator - 1);
-        if (isAlphaNumeric(driveLetter) == false) {
-            return ERROR_BAD_PATHNAME;
-        }
+        driveLetter = drivePathStart;
         pathStart = firstAlphaNumericCharacter(driveSeparator);
     } else {
-        pathStart = firstAlphaNumericCharacter(input);
+        pathStart = drivePathStart;
     }
 
     // At this point pathStart should look like "foo\bar.txt"
-    // The variable driveLetter will contain the symbolically linked partition letter unless we
+    // The variable driveLetter will contain the symbolically linked partition string unless we
     // were supplied a relative path. If we were supplied a relative path, determine the currently running xbe directory
     // and use this path to prefix. If that fails, we fallback to CdRom0.
-    if (driveLetter != '\0') {
-        if (symbolicLinkToXboxPath(driveLetter, output, MAX_PATH) == false)
+    if (driveLetter != NULL) {
+        int driveLetterLen = driveSeparator - drivePathStart;
+        if (symbolicLinkToXboxPath(driveLetter, driveLetterLen, output, MAX_PATH) == false)
         {
             // Not mounted or invalid drive letter
             return ERROR_INVALID_DRIVE;
         }
+    // Path didnt have a ":" so it doesnt have a driveLetter. Fallback to relative path processing
     } else {
         nxGetCurrentXbeNtPath(output);
-        // nxGetCurrentXbeNtPath returns \Device\*\foo\bar.xbe. So drop the last file name.
+        // nxGetCurrentXbeNtPath returns \Device\*\foo\bar.xbe. So drop the last file name to get \Device\*\foo\
         // First check for the last \. If that fails try a /.
-        char * lastSlash = strrchr(output, '\\');
+        char *lastSlash = NULL;
+        if (lastSlash == NULL) {
+            lastSlash = strrchr(output, '\\');
+        }
         if (lastSlash == NULL) {
             lastSlash = strrchr(output, '/');
         }
-
         if (lastSlash) {
             lastSlash[1] = '\0';
         }
+        // If it couldn't find a slash, something is wrong. Fallback to the CdRom0 path.
         else
         {
-            // If it couldn't find a slash, something is wrong. Fallback to the CdRom0 path.
             strcpy(output, "\\Device\\CdRom0\\");
         }
     }
