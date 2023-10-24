47e029ea7fd392c94b4a517b385381928a82faf4
Ryzee119 <wendland@live.com.au>
2023-10-24 18:52:48 +1030

    widgets: Add string helpers

---
+++ src/lvgl_widgets/helpers.h
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+// SPDX-FileCopyrightText: 2022 Ryzee119
+
+#ifndef _HELPERS_H
+#define _HELPERS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "../lithiumx.h"
+
+lv_obj_t *lv_strdup(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
